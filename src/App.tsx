f97c04b34c1763edef6dccb04b0e575f0b4fd209
Gabriel Henrique <gabrieltjs75@gmail.com>
2025-05-31 10:17:18 -0300

    23. Criando componente de menu com links e ícones

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { Container } from './componets/Container';
-import { Heading } from './componets/Heading';
 import { Logo } from './componets/Logo';
+import { Menu } from './componets/Menu';
 
 import './styles/theme.css';
 import './styles/global.css';
+
 export function App() {
   return (
     <>
       <Container>
         <Logo />
       </Container>
+
       <Container>
-        <Heading>Menu</Heading>
+        <Menu />
       </Container>
     </>
   );
