4ccc7e212222c4c63c2611d53495d68979433fec
Gabriel Henrique <gabrieltjs75@gmail.com>
2025-10-09 15:29:41 -0300

    44. Estado global da aplicação com useState e comunicação via props

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,23 @@
 import { Container } from '../../componets/Container';
 import { CountDown } from '../../componets/CountDown';
 import { MainForm } from '../../componets/MainForm';
+import type { TaskStateModel } from '../../models/TaskStateModel';
 import { MainTemplate } from '../../templates/MainTemplate';
 
-export function Home() {
+type HomeProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+export function Home(props: HomeProps) {
+  const { state, setState } = props;
+
   return (
     <MainTemplate>
       <Container>
         <CountDown />
       </Container>
+
       <Container>
         <MainForm />
       </Container>
