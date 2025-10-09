1cc90d6132428fa416b9a47ef9d4a69f77038db6
Gabriel Henrique <gabrieltjs75@gmail.com>
2025-10-09 16:05:17 -0300

    46. O conceito de prop drilling (talvez você precise de Context API)

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,23 +1,17 @@
 import { Container } from '../../componets/Container';
 import { CountDown } from '../../componets/CountDown';
 import { MainForm } from '../../componets/MainForm';
-import type { TaskStateModel } from '../../models/TaskStateModel';
 import { MainTemplate } from '../../templates/MainTemplate';
 
-export type HomeProps = {
-  state: TaskStateModel;
-  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
-};
-
-export function Home(props: HomeProps) {
+export function Home() {
   return (
     <MainTemplate>
       <Container>
-        <CountDown {...props} />
+        <CountDown />
       </Container>
 
       <Container>
-        <MainForm {...props} />
+        <MainForm />
       </Container>
     </MainTemplate>
   );
